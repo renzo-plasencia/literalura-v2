a51c41ab079d1439d651865adaa1a7418e3b6bb8
renzo <renzoplas06@gmail.com>
2025-01-08 00:24:20 -0500

    First commit

---
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -0,0 +1,21 @@
+package com.aluracursos.literalura;
+
+import main.Main;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.muestraMenu();
+	}
+
+}
